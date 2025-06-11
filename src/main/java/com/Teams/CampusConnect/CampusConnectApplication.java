e49c893933e21e4f0c2e25d1b22b387489f8bf8e
rohanmandal341 <146451203+rohanmandal341@users.noreply.github.com>
2025-06-11 18:45:21 +0530

    Initial commit for CampusConnect backend

---
+++ src/main/java/com/Teams/CampusConnect/CampusConnectApplication.java
@@ -0,0 +1,13 @@
+package com.Teams.CampusConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusConnectApplication.class, args);
+	}
+
+}
